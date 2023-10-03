15ebe39ae91529a53865ccbf429f05ab4849de90
lai2-01 <lai2-01@itismeucci.com>
2023-10-03 10:42:59 +0200

    ciao2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,36 @@
 package com.example;
-
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int risposta=0;
+        try{
+            double casuale = (int)(Math.random()*100);
+            System.out.println( "server avviato" );
+        ServerSocket server = new ServerSocket(3000);
+
+        Socket s =server.accept();
+        System.out.println( "un cliente si è connesso");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        
+        out.writeBytes("indovina un numero da 1 a 100" + '\n');
+        do{
+            
+        }while(risposta==3);
+
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istanza del server del server");
+        }
     }
 }
