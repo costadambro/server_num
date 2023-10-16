f237054fb65c29e79f8f2cd6d90f3f92e582992d
lai2-24 <lai2-24@itismeucci.com>
2023-10-16 10:19:40 +0200

    final commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,43 +1,46 @@
 package com.example;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-public class App 
-{
-    public static void main( String[] args )
-    {
-        int risposta=0;
-        try{
-            double casuale = (int)(Math.random()*100);
-            System.out.println( "server avviato" );
+
+public class App {
+    public static void main(String[] args) {
+        int risposta = 0;
+        int conta = 0;
+        int client = 0;
+        try {
+            double casuale = (int) (Math.random() * 100);
+            System.out.println("server avviato");
             ServerSocket server = new ServerSocket(3000);
 
-            Socket s =server.accept();
-            System.out.println( "un cliente si è connesso");
+            Socket s = server.accept();
+            System.out.println("un cliente si è connesso");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
-        
+
             out.writeBytes("indovina un numero da 1 a 100" + '\n');
-            do{
-                if(casuale==(Integer.parseInt(in.readLine()))){
-                    risposta=3;
-                }
-                else if(casuale>(Integer.parseInt(in.readLine()))){
-                    risposta=2;
-                }
-                else if(casuale>(Integer.parseInt(in.readLine()))){
-                    risposta=1;
+            do {
+                client = Integer.parseInt(in.readLine());
+                conta++;
+                if (casuale == client) {
+                    risposta = 3;
+                    conta--;
+                } else if (casuale > client) {
+                    risposta = 2;
+                } else if (casuale < client) {
+                    risposta = 1;
                 }
                 out.writeBytes(Integer.toString(risposta) + '\n');
-            }while(risposta!=3);
+                out.writeBytes(Integer.toString(conta) + '\n');
+            } while (risposta != 3);
             System.exit(0);
 
-
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.toString());
             System.out.println("Errore durante l'istanza del server del server");
         }
     }
